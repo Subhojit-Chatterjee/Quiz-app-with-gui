944d86f422658d29685cc1089947ba749dcc4c6f
Subhojit-Chatterjee <62737899+Subhojit-Chatterjee@users.noreply.github.com>
2024-12-11 12:18:11 +0530

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+QUES_FONT = ("Arial", 20, "italic")
+SCORE_FONT = ("Arial", 12, "bold")
+
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quizler")
+        self.window.config(padx=50, pady=50, bg=THEME_COLOR)
+
+        self.score_label = Label(text=f"Score: {self.quiz.score}",
+                                 foreground="white", bg=THEME_COLOR, font=SCORE_FONT)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, text="Question text",
+                                                     fill=THEME_COLOR, font=QUES_FONT, width=280)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
+
+        self.true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=self.true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="That is the end of the quiz. "
+                                                            f"Your final score is"
+                                                            f" {self.quiz.score}/{len(self.quiz.question_list)}")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, user_is_right):
+        if user_is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, func=self.get_next_question)
